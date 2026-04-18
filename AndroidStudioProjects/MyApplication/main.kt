3c6709b882ebd314f0e59147d379054547165195
Thakur <singhshauryapratap753>
2026-04-18 11:24:41 +0530

    abhi tak ki all working projects

---
+++ AndroidStudioProjects/MyApplication/main.kt
@@ -0,0 +1,115 @@
+package com.example.myapplication.kotlin
+
+fun main () {
+
+    /*
+airthmetics operator
+ int  a = 41486787.0
+var b=5648678714867.0
+
+var c = a+b
+var d = a*b
+var e = a/b
+
+println("answer is $d")
+println("answer is $c")
+println("answer is $e")
+comperasion operator
+var a = 9
+var b = 8
+println('a' == 'b')
+println ('a' != 'b')
+println('a' < 'b')
+
+logical operators
+var isAdult = true
+var hasLicense = true
+var age = 19
+println("they can drive : ${isAdult && hasLicense }")
+if-else operators
+
+var isAdult = true
+if (isAdult){
+println("YES")
+}else{
+println("NO")
+}
+}
+var score = 50
+if (score >= 100){
+println("A")
+}else if (score >= 80){
+println("B")
+}else if (score >= 70){
+println("C")
+}else if (score >= 60){
+println("D")
+}else{
+println("F")
+}
+}
+for loop
+for (i in 1..100){
+println(i)
+}
+while loop
+    var n = 0
+    while ( n < 10) {
+        println(n)
+        n++
+    }
+}
+ do while loop
+    var n = 0
+    do {
+        println(n)
+        n++
+    } while (n < 10)
+// when with user input (readline)
+
+    println("enter a month number")
+
+    var month = readLine().toString().toInt()
+    when (month) {
+        1 -> println("January")
+        2 -> println("February")
+        3 -> println("March")
+        4 -> println("April")
+        5 -> println("May")
+    }
+*/
+    // collections
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
